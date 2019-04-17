9d11ccd3bcf1298c4bacb420ea0d3436be2a0438
baosongle <songlebao@outlook.com>
2019-04-17 21:56:09 +0800

    TreeGenerator 中，默认生成的树中没有重复值的节点

--- src/main/java/support/tree/TreeGenerator.java
+++ src/main/java/support/tree/TreeGenerator.java
@@ -1,27 +1,37 @@
 package support.tree;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class TreeGenerator {
 	private Random random;
+	private boolean repeated;
 	private int numberOfNodes;
 	private int minVal;
 	private int maxVal;
+
+	private Set<Integer> numbers = new HashSet<>();
 
 	public TreeGenerator(int numberOfNodes, int minVal, int maxVal) {
 		this.random = new Random();
 		this.numberOfNodes = numberOfNodes;
 		this.minVal = minVal;
 		this.maxVal = maxVal;
+		this.repeated = false;
 	}
 
 	public TreeGenerator(int numberOfNodes, int minVal, int maxVal, int seed) {
-        this.random = new Random(seed);
+		this(numberOfNodes, minVal, maxVal, seed, false);
+	}
+
+	public TreeGenerator(int numberOfNodes, int minVal, int maxVal, int seed, boolean repeated) {
+		this.random = new Random(seed);
 		this.numberOfNodes = numberOfNodes;
 		this.minVal = minVal;
 		this.maxVal = maxVal;
+		this.repeated = repeated;
 	}
-
 
 	public TreeNode randomTree() {
         TreeNode root = new TreeNode(randomVal());
@@ -32,6 +42,13 @@ public class TreeGenerator {
 	}
 
 	private int randomVal() {
-		return random.nextInt(maxVal - minVal) + minVal;
+		int val = random.nextInt(maxVal - minVal) + minVal;
+		if (repeated)
+			return val;
+		if (!numbers.contains(val)) {
+			numbers.add(val);
+			return val;
+		}
+		return randomVal();
 	}
 }
--- src/test/java/support/tree/TestTreeTransverse.java
+++ src/test/java/support/tree/TestTreeTransverse.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class TestTreeTransverse {
 	@Test
 	public void testPreOrderTransverse() {
-		TreeGenerator generator = new TreeGenerator(10, 0, 100, 1);
+		TreeGenerator generator = new TreeGenerator(15, 0, 100, 1);
 		TreeNode root = generator.randomTree();
 		TreeTransverse.preOrderTransverse(root);
 		System.out.println();
@@ -13,7 +13,7 @@ public class TestTreeTransverse {
 
 	@Test
 	public void testInOrderTransverse() {
-		TreeGenerator generator = new TreeGenerator(10, 0, 100, 1);
+		TreeGenerator generator = new TreeGenerator(15, 0, 1000);
 		TreeNode root = generator.randomTree();
 		TreeTransverse.inOrderTransverse(root);
 		System.out.println();
@@ -21,7 +21,7 @@ public class TestTreeTransverse {
 
 	@Test
 	public void testPostOrderTransverse() {
-		TreeGenerator generator = new TreeGenerator(10, 0, 100, 1);
+		TreeGenerator generator = new TreeGenerator(15, 0, 100, 1);
 		TreeNode root = generator.randomTree();
 		TreeTransverse.postOrderTransverse(root);
 		System.out.println();
